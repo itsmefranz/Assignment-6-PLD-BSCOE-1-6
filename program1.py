8d59a4826036e8a954a78214c9e908b1d003c422
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-20 21:08:17 +0800

    added input functions for 4 numbers enclosed in variables

---
+++ program1.py
@@ -0,0 +1,12 @@
+#Assignment 6: Program 1
+def hi_to_low(fig1, fig2, fig3, fig4)
+
+
+#Create a program that ask 4 numbers. 
+num1= float(input("Enter first number here: "))
+num2= float(input("Enter second number here: "))
+num3= float(input("Enter third number here: "))
+num4= float(input("Enter fourth number here: "))
+
+#Print the 4 numbers from highest to lowest using only if-else statement.
+hi_to_low(num1, num2, num3, num4)
