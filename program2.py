874a08bd72113e2afb369dd018eda0a954f6f39a
Daniella Francine Velasquez <daniellafrancine.velasquez@gmail.com>
2021-11-24 11:11:10 +0800

    added introduction for the quiz asking user to input name and then will print a message

---
+++ program2.py
@@ -0,0 +1,10 @@
+from random import randint
+
+def intro():
+    print("Welcome to my Math Quiz!")
+    name= input("Please enter your name: ")
+    print(f"Hi, {name}! All the best!")
+    return name
+
+
+intro()
